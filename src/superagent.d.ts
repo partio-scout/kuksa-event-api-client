8444d80d1df3f526cf531ec05d8eca6e3afefdd5
Emil Virkki <emil.virkki@gmail.com>
2018-06-06 21:06:29 +0300

    Remove superagent-as-promised

--- src/superagent.d.ts
+++ src/superagent.d.ts
@@ -3,11 +3,6 @@ declare module 'superagent' {
   export = superagent;
 }
 
-declare module 'superagent-as-promised' {
-  function superagentAsPromised(superagent: any): any;
-  export = superagentAsPromised;
-}
-
 declare module 'superagent-proxy' {
   function superagentProxy(superagent: any): any;
   export = superagentProxy;
--- src/superagent.ts
+++ src/superagent.ts
@@ -1,8 +1,6 @@
 /// <reference path="superagent.d.ts" />
 import superagent = require('superagent');
-import superagentAsPromised = require('superagent-as-promised');
 import superagentProxy = require('superagent-proxy');
 
-superagentAsPromised(superagent);
 superagentProxy(superagent);
 export const request = superagent;
